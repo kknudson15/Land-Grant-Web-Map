092ec60c8fec4a8026c4609c2ac852c8abed0e62
Kyle Knudson <kyle.knudson2015@gmail.com>
2019-01-10 09:21:40 -0600

    change data file and read data for markers on map

--- map.py
+++ map.py
@@ -2,15 +2,19 @@ import folium
 import pandas as pd 
 
 
-cities = pd.read_csv('worldcities.csv')
+colleges = pd.read_csv('landgrantcolleges.csv')
 
-print(cities.head())
+print(colleges.head())
 
-lat = list(cities['lat'])
-lng = list(cities['lng'])
+name = list(colleges['instnm'])
+lat = list(colleges['LATITUDE (HD2017)'])
+lng = list(colleges['LONGITUD (HD2017)'])
 map = folium.Map(location = [40,-94], zoom_start = 5, tiles = "Mapbox Bright")
 
 fg = folium.FeatureGroup(name = 'My Map')
+
+for lt, ln, nm in zip (lat, lng, name):
+    fg.add_child(folium.Marker(location = [lt,ln], popup = f'{nm}', icon = folium.Icon(color = 'blue')))
 
 fg.add_child(folium.Marker(location = [45.548611, -94.151944], popup = "Harvard of Central Minnesota", icon = folium.Icon(color = 'red')))
 fg.add_child(folium.Marker(location = [44.928121, -93.3474], popup = "Kyle's Home", icon = folium.Icon(color = 'green')))
