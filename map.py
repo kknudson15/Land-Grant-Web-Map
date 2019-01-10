0405163660847ee0eaed3f403f992f3f6f5e070d
Kyle Knudson <kyle.knudson2015@gmail.com>
2019-01-10 08:35:58 -0600

    adds some markers to the map

--- map.py
+++ map.py
@@ -1,6 +1,8 @@
 import folium 
 
-map = folium.Map(location = [45,-94], zoom_start = 10, tiles = "Mapbox Bright")
+map = folium.Map(location = [40,-94], zoom_start = 5, tiles = "Mapbox Bright")
+map.add_child(folium.Marker(location = [45.548611, -94.151944], popup = "Harvard of Central Minnesota", icon = folium.Icon(color = 'red')))
+map.add_child(folium.Marker(location = [44.928121, -93.3474], popup = "Kyle's Home", icon = folium.Icon(color = 'green')))
 
 
 map.save("Map1.html")
